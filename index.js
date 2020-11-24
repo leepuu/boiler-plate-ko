84d080c4e4fdf715cbc3dbf63ddb4a729de3e255
leepuu <leepuu001@naver.com>
2020-11-24 13:54:02 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+//application/json
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 
 mongoose
-	.connect("mongodb+srv://leepuu:qwe57866130@boilerplate.vkbdp.mongodb.net/test?retryWrites=true&w=majority", {
+	.connect(config.mongoURI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
@@ -15,7 +24,21 @@ mongoose
 	.catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-	res.send("Hello World!~~~~");
+	res.send("Hello World!~~집에갈래~~");
+});
+
+app.post("/register", (req, res) => {
+	//회원가입 할때 필요한 정보들을 client에서 가져오면
+	//그것들을 데이터 베이스에 넣어줌
+
+	const user = new User(req.body);
+
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true,
+		});
+	});
 });
 
 app.listen(port, () => {
